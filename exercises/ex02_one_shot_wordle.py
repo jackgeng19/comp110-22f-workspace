370c4b66df5404abe952152501adb31767a503b3
Qicheng Geng <qgeng@unc.edu>
2022-09-08 17:58:49 -0400

    Progress on Ex03 on Sept 8

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,8 +1,7 @@
+"""EX02 - Wordle."""
+
 __author__ = "730433734"
 
-from operator import index
-
-#Code declaration
 secret_word: str = "python" 
 guess_word: str = input(f"What is your {len(secret_word)}-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
@@ -11,32 +10,30 @@ YELLOW_BOX: str = "\U0001F7E8"
 i: int = 0
 result: str = ""
 
-#When the lengths do not match
+"""When the lengths do not match"""
 while len(guess_word) != len(secret_word):
     guess_word = input(f"That was not {len(secret_word)} letters! Try again: ")
 
-#When the lengths match
+"""When the lengths match"""
 if guess_word != secret_word:
     while i < len(secret_word):
         if guess_word[i] == secret_word[i]:
             result += GREEN_BOX
-        #When two characters do not match
         elif guess_word[i] != secret_word[i]:
+            """When two characters do not match"""
             is_exist: bool = False
-            #Alternative index
             j: int = 0
-            while is_exist == False and j < len(secret_word):
+            while is_exist is False and j < len(secret_word):
                 if guess_word[i] == secret_word[j]:
                     is_exist = True
                 j += 1
-            if is_exist == False:
+            if is_exist is False:
                 result += WHITE_BOX
             else:
                 result += YELLOW_BOX
         i += 1
     print(result)
     print("Not quite. Play again soon!")
-#When two words match perfectly
 elif guess_word == secret_word:
     result = GREEN_BOX * len(secret_word)
     print(result)
---
+++ exercises/ex03_wordle.py
@@ -0,0 +1,63 @@
+"""EX03 - Wordle."""
+
+__author__ = "730433734"
+
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+def contains_char(word: str, char: str) -> bool:
+    """Return True if char is found at any index of word, and return False otherwise"""
+    assert len(char) == 1
+    i: int = 0
+    while i < len(word):
+        if word[i] == char:
+            return True
+        else:
+            i += 1
+    return False
+
+def emojified(guess_word: str, secret_word: str) -> str:
+    """Return a string of emoji whose color codifies the same as previously implemented in EX02"""
+    assert len(guess_word) == len(secret_word)
+    result: str = ""
+    i: int = 0
+    while i < len(secret_word):
+        if secret_word[i] == guess_word[i]:
+            result += GREEN_BOX
+        elif contains_char(secret_word, guess_word[i]):
+            result += YELLOW_BOX
+        else:
+            result += WHITE_BOX
+        i += 1
+    return result
+
+def input_guess(n: str) -> str:
+    """Continue prompting users until they provide a guess of the expected length."""
+    input_word: str = input(f"Enter a {n} character word: ")
+    while True:
+        if len(input_word) != n:
+            input_word = input(f"That wasn't {n} chars! Try again: ")
+        elif len(input_word) == n:
+            return input_word
+
+def main() -> None:
+    """The entrypoint of the program and main game loop."""
+    secret_word: str = "codes"
+    current_turn: int = 1
+    won: bool = False
+    while current_turn <= 6 and won is False:
+        print(f"=== Turn {current_turn}/6 ===")
+        guess_word = input_guess(len(secret_word))
+        print(emojified(guess_word, secret_word))
+        if guess_word == secret_word:
+            print(f"You won in {current_turn}/6 turns!")
+            won = True
+        elif current_turn == 6:
+            print("X/6 - Sorry, try again tomorrow!")
+            current_turn += 1
+        else:
+            current_turn += 1
+
+if __name__ == "__main__":
+    main()
